291c6cca91ccae6a6d0d17da0416926d0389784d
Anuran Pradhan <anuranpradhan002@gmail.com>
2025-03-10 00:08:02 +0530

    ReverseArray

---
+++ Array/RevereArray.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class RevereArray {
+    public static int[] reverse(int []arr,int start,int end){
+if(arr.length==1){
+    return arr;
+}
+if(end<=start){
+    return arr;
+}
+int temp=arr[start];
+arr[start]=arr[end];
+arr[end]=temp;
+return reverse(arr, start+1, end-1);
+    }
+public static void main(String[] args) {
+    int arr[]={1,2,5,6,8,4,6};
+reverse(arr, 0, arr.length-1);
+System.out.println(Arrays.toString(arr));
+}
+}
